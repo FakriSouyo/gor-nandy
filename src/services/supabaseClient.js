6993a174289414f711e8451dd59637784641b790
FakriSouyo <fakriggwp@gmail.com>
2024-07-26 02:06:37 +0700

    changes

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Mengambil variabel lingkungan dari proses
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// Membuat klien Supabase
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
