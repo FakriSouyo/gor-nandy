98f0a6ce875a4cf1bfc1267fe690de3a87a86d7a
FakriSouyo <fakriggwp@gmail.com>
2024-07-26 02:05:11 +0700

    changes wtf i dont use env

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://aamcsurfqoltdvnnrckf.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFhbWNzdXJmcW9sdGR2bm5yY2tmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE0MTA5NjgsImV4cCI6MjAzNjk4Njk2OH0.D10X92_9eij4jMxmUGODKEjflGsjZ2DsyINI1-QYvA4';
+// Mengambil variabel lingkungan dari proses
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+// Membuat klien Supabase
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
