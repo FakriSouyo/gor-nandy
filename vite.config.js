7331f1e78f0eed1b386fa93d0da663f2c84e482e
FakriSouyo <fakriggwp@gmail.com>
2024-07-27 19:07:17 +0700

    commit

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,20 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+    open: true,
+  },
 })
